1532ad50c60f69a9ba218e74eaee1ad4a396d050
Rolkarolka <romanowskarolina@gmail.com>
2020-11-12 22:25:27 +0100

    graficzn wersja 1.1

--- graphic.py
+++ graphic.py
@@ -3,7 +3,7 @@ import pygame as pg
 class TicTacToe:
     def __init__(self, first_x=True):
         pg.init()
-        pg.display.set_caption("TicTacToe") 
+        pg.display.set_caption("TicTacToe")
         self.WIDTH = 600
         self.HEIGHT = self.WIDTH
         self.screen = pg.display.set_mode((self.WIDTH, self.HEIGHT))
@@ -33,6 +33,8 @@ class TicTacToe:
         pg.draw.line(self.screen, self.board_lines_colour, (self.WIDTH // 3 * 2, 0), (self.WIDTH // 3 * 2, self.HEIGHT), 5)
         pg.draw.line(self.screen, self.board_lines_colour, (0, self.HEIGHT // 3), (self.WIDTH, self.HEIGHT // 3), 5)
         pg.draw.line(self.screen, self.board_lines_colour, (0, self.HEIGHT // 3 * 2), (self.WIDTH, self.HEIGHT // 3 * 2), 5)
+
+    def draw_movements(self):
         for x_row in range(3):
             for y in range(3):
                 if self.board[x_row][y] == 1:
@@ -41,8 +43,7 @@ class TicTacToe:
                 elif self.board[x_row][y] == 0:
                     coord = self.fields_start[x_row][y]
                     self.draw_o(coord[0], coord[1])
-        pg.display.flip()
-    
+
     def find_fild(self, coord):
         if coord < self.WIDTH // 3:
             return 0
@@ -51,25 +52,44 @@ class TicTacToe:
         else:
             return 2
 
+    def draw_horizontal_winning_line(self, x_row):
+        y = self.fields_start[self.board.index(x_row)][0][1] + self.HEIGHT // 6
+        self.draw_movements()
+        pg.draw.line(self.screen, (255, 0, 0), (0, y), (self.WIDTH, y), 5)
+
+    def draw_vertical_winning_line(self, i):
+        x = self.fields_start[0][i][0] + self.WIDTH // 6
+        self.draw_movements()
+        pg.draw.line(self.screen, (255, 0, 0), (x, 0), (x, self.HEIGHT), 5)
+
     def check_winning(self):
+        self.draw_movements()
         # form a horizontal line
         for x_row in self.board:
             if all([x is self.player for x in x_row]):
+                self.draw_horizontal_winning_line(x_row)
                 return self.player
         # form a vertical line
         for i in range(len(self.board[0])):
             if all([x[i] is self.player for x in self.board]):
+                self.draw_vertical_winning_line(i)
                 return self.player
         # form a diagonal line from the upper-left to the lower-right corner
         if all([self.board[i][i] is self.player for i in range(len(self.board))]):
+            pg.draw.line(self.screen, (255, 0, 0), (0, 0), (self.WIDTH, self.HEIGHT), 10)
             return self.player
         # form a diagonal line from the lower-left to the upper-right corner
         if all([self.board[i][2 - i] is self.player for i in range(2, -1, -1)]):
+            pg.draw.line(self.screen, (255, 0, 0), (0, self.HEIGHT), (self.WIDTH, 0), 10)
             return self.player
         # if draw
         if all([all([x[i] is not None for i in range(len(self.board))]) for x in self.board]):
             return -1
         return None
+
+    def end_game(self, result):
+        pg.time.delay(1000)
+        pg.quit()
 
     def update_board(self, pos):
         y = self.find_fild(pos[1])
@@ -78,23 +98,20 @@ class TicTacToe:
             self.board[y][x] = self.player
             result = self.check_winning()
             if result is not None:
-                return result
+                pg.display.update()
+                self.end_game(result)
             self.player = int(not self.player)
-        self.draw_board()
-        return None
 
     def game(self):
         while self.run:
             self.draw_board()
+            pg.display.update()
             for event in pg.event.get():
                 if event.type == pg.MOUSEBUTTONUP:
                     pos = pg.mouse.get_pos()
-                    result = self.update_board(pos)
-                    if result is not None:
-                        self.run = False
+                    self.update_board(pos)
                 if event.type == pg.QUIT:
                     self.run = False
         pg.quit()
-        return result
 
 tic = TicTacToe()
